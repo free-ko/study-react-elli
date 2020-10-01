cd62b1edf88a58406dc845f2d43aa561ec62bf9d
kyw <[duddnr654@naver.com]>
2020-10-01 13:57:59 +0900

    🌈 4.9 이벤트 처리하기 (State 업데이트)

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -9,11 +9,40 @@ class Habits extends Component {
       { id: 3, name: "Coding", count: 0 },
     ],
   };
+
+  handleIncrement = (habit) => {
+    // this.state.habits[index].count++ 이렇게 작성하면 React가 바보라서 모릅니다.
+    const habits = [...this.state.habits]; // habits에 있는 Object를 복사해서 habits에 넣습니다. 복사해 주는 이유는 State에 있는 Data를 부분적으로 업데이트 하면  React가 바보라서? 모릅니다. 그래서 우리가 State를 전체적으로 업데이트 해주어야 합니다.
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    // 선택되어진 habit만 빼고 state를 업데이트 합니다.
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <Habit key={habit.id} habit={habit} />
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
     );
