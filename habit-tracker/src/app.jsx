315f4d628f01bf7ffe66204d382c4a2d5de047bd
kyw <[duddnr654@naver.com]>
2020-10-03 18:15:02 +0900

    4.11 Navbar 컴포넌트 만들기

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import "./app.css";
 import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    // this.state.habits[index].count++ 이렇게 작성하면 React가 바보라서 모릅니다.
+    // habits에 있는 Object를 복사해서 habits에 넣습니다.
+    // 복사해 주는 이유는 State에 있는 Data를 부분적으로 업데이트 하면  React가 바보라서? 모릅니다.
+    // 그래서 우리가 State를 전체적으로 업데이트 해주어야 합니다.
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    // 선택되어진 habit만 빼고 state를 업데이트 합니다.
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
 }
+
 export default App;
