30e428555d0d87000135a292193fa0446526e446
free-ko <duddnr654@naver.com>
2020-10-14 15:32:49 +0900

    Error Delete

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { memo } from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+const VideoItem = memo(({ video, video: { snippet }, onVideoClick, display }) => {
 
   const displayType = display === 'List' ? styles.list : styles.grid;
 
@@ -20,6 +20,6 @@ const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
     </div>
   </li>
   );
-};
+});
 
 export default VideoItem;
