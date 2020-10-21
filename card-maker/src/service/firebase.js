98a4c3da927f407be9be53f6b878f81e5e6ae5f4
free-ko <duddnr654@naver.com>
2020-10-21 17:45:20 +0900

    10.2 Firebase Login 기능 추가

--- card-maker/src/service/firebase.js
+++ card-maker/src/service/firebase.js
@@ -1,8 +1,13 @@
-  const firebaseConfig = {
-    apiKey: process.env.REAC_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REAC_APP_FIREBASE_AUTH-DOMAIN,
-    databaseURL: process.env.REAC_APP_FIREBASE_DB_URL,
-    projectId: process.env.REAC_APP_FIREBASE_PROJECT_DI
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
