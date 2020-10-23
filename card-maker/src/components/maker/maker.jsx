dcaf6a67ccd0640c9637c43537ad77cf44cc37a2
free-ko <duddnr654@naver.com>
2020-10-23 17:14:12 +0900

    10.9 Maker Layout

--- card-maker/src/components/maker/maker.jsx
+++ card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
@@ -7,6 +7,41 @@ import Header from '../header/header';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'FREEKO_1',
+      company: 'FREEKO',
+      theme: 'light',
+      title: 'UX Engineer',
+      email: 'duddnr654@naver.com',
+      message: 'go for it',
+      fileName: 'FREEKO',
+      fileURL: 'FREEKO.png'
+    },
+    {
+      id: '2',
+      name: 'FREEKO_2',
+      company: 'FREEKO',
+      theme: 'light',
+      title: 'UX Engineer',
+      email: 'duddnr654@naver.com',
+      message: 'go for it',
+      fileName: 'FREEKO',
+      fileURL: 'FREEKO.png'
+    },
+    {
+      id: '3',
+      name: 'FREEKO_3',
+      company: 'FREEKO',
+      theme: 'light',
+      title: 'UX Engineer',
+      email: 'duddnr654@naver.com',
+      message: 'go for it',
+      fileName: 'FREEKO',
+      fileURL: 'FREEKO.png'
+    }
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +58,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor/>
-        <Preview/>
+        <Editor cards={cards}/>
+        <Preview cards={cards}/>
       </div>
       <Footer />
     </section>
