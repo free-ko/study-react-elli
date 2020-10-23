baa6035ade3709d66616669ff20a7341f6a2d44a
free-ko <duddnr654@naver.com>
2020-10-23 16:40:00 +0900

    add editor and preview layout

--- card-maker/src/components/maker/maker.jsx
+++ card-maker/src/components/maker/maker.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './maker.module.css';
@@ -20,6 +22,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor/>
+        <Preview/>
+      </div>
       <Footer />
     </section>
   );
