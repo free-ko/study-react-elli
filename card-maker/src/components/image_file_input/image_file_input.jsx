b6d9b1223048644cc4ead2c55a21220c427e1f7e
free-ko <duddnr654@naver.com>
2020-10-25 16:51:29 +0900

    add card and edit form component

---
+++ card-maker/src/components/image_file_input/image_file_input.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import sytles from './image_file_input.module.css';
+
+const imageFileInput = (props) => (
+        <button>Image</button>
+    );
+
+export default imageFileInput;
