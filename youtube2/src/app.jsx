6820a3cdc5297b56b7b931caf6c1016ef59fd736
free-ko <duddnr654@naver.com>
2022-08-13 21:17:43 +0900

    [FEAT] Youtube API 데이터 호출 로직 구현

--- youtube2/src/app.jsx
+++ youtube2/src/app.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './app.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDr__XM5Fe4Y8yzUAqFoQFu3iUmSjYcDtE",
+        requestOptions
+    )
+        .then(response => response.json())
+        .then(result => setVideos(result.items))
+        .catch(error => console.log('Error', error));
+  }, [])
+
   return (
     <h1>Hello:)</h1>
   );
