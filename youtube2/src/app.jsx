b5f17c6a2683cd8ddd556d08cd3aa4682f7a593b
free-ko <duddnr654@naver.com>
2022-08-14 11:10:11 +0900

    [FEAT] Youtube Service 로직 구현

--- youtube2/src/app.jsx
+++ youtube2/src/app.jsx
@@ -5,38 +5,17 @@ import SearchHeader from "./components/searchHeader/searchHeader";
 
 import styles from  './app.module.css';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDr__XM5Fe4Y8yzUAqFoQFu3iUmSjYcDtE`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => result.items.map(item => ( { ...item, id: item.id.videoId } )))
-      .then(items => setVideos(items))
-      .catch(error => console.log('Error', error));
+    youtube.search(query)
+      .then(videos => setVideos(videos));
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDr__XM5Fe4Y8yzUAqFoQFu3iUmSjYcDtE",
-        requestOptions
-    )
-        .then(response => response.json())
-        .then(result => setVideos(result.items))
-        .catch(error => console.log('Error', error));
+    youtube.mostPopular()
+      .then(videos => setVideos(videos));
   }, [])
 
   return (
